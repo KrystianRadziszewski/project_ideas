3fffbccf3fafd18a95c158f4ab2a834b50330da6
Krystian Radziszewski <krystian-radziszewski-it@o2.pl>
2023-09-19 11:58:49 +0200

    post card edit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -23,20 +23,20 @@ const PostCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 		<div className="prompt_card">
 			<div className=" flex justify-between items-start gap-5">
 				<div className="flex-1 flex justify-start items-center gap-3 ">
-					{post.creator && post.creator.image && <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain" />}
-					{/* <Image src={post.creator.image} alt="user_image" width={40} height={40} className=" rounded-full object-contain" /> */}
+					{/* {post.creator && post.creator.image && <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain" />} */}
+					<Image src={post.creator.image} alt="user_image" width={40} height={40} className=" rounded-full object-contain" />
 
 					<div className="flex flex-col">
-						{post.creator && post.creator.username && (
+						{/* {post.creator && post.creator.username && (
 							<h3 className="font-satoshi font-semibold text-gray-900">
 								<span className="font-thin text-sm text-gray-400">Created by: </span>
 								{upChar(post.creator.username)}
 							</h3>
-						)}
-						{/* <h3 className="font-satoshi font-semibold text-gray-900">
+						)} */}
+						<h3 className="font-satoshi font-semibold text-gray-900">
 							<span className=" font-thin text-sm text-gray-400">Created by: </span>
 							{upChar(post.creator.username)}
-						</h3> */}
+						</h3>
 					</div>
 				</div>
 
